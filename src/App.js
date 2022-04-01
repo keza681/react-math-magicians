4b0cbd27966471adb352880acbce6ad93759e44f
keza681 <lindakeza19@gmail.com>
2022-04-01 19:58:32 +0200

    Update the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import Calculator from './Component/calculator';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="mainDiv">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
+      </div>
+    );
+  }
 }
-
-export default App;
