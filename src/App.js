e4e974ee6b35c124a4de49378a98eef78a94a0c5
keza681 <lindakeza19@gmail.com>
2022-04-01 20:28:09 +0200

    Resolve conflict problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
-import NavBar from './components/NavBar';
+import Home from './component/Home';
+import Calculator from './component/Calculator';
+import Quote from './component/Quote';
+import NavBar from './component/NavBar';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="mainDiv">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Calculator" element={<Calculator />} />
-          <Route path="/Quote" element={<Quote />} />
-        </Routes>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="mainDiv">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
+    </div>
+  );
 }
+export default App;
