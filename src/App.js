687c4f02d3e7bfd9d9f5881783d49d58a0e3b45f
keza681 <lindakeza19@gmail.com>
2022-04-01 20:40:10 +0200

    Update develop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
-import Calculator from './Component/calculator';
+import { Routes, Route } from 'react-router-dom';
+import Home from './component/Home';
+import Calculator from './component/Calculator';
+import Quote from './component/Quote';
+import NavBar from './component/NavBar';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="mainDiv">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
+    </div>
+  );
 }
+export default App;
